34fdf2469b576ad7d84f6e4508c8f2be62e153a1
Claudio128 <cllaudio128@gmail.com>
2024-01-13 21:56:30 -0300

    Introdução ao TS

---
+++ Aula01/index.js
@@ -0,0 +1,9 @@
+var fisrtName = "Claudio";
+var anotheName = 1;
+var x = true;
+function greeting(name) {
+    console.log("Olá " + name);
+}
+greeting(fisrtName);
+//greeting(anotheName)
+//greeting(x)
---
+++ Aula01/index.ts
@@ -0,0 +1,11 @@
+const fisrtName = "Claudio"
+const anotheName = 1
+const x = true
+
+function greeting(name: string) {
+    console.log("Olá " + name)
+}
+
+greeting(fisrtName)
+//greeting(anotheName)
+//greeting(x)
