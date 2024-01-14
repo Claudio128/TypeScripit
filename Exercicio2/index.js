361382d30180db327aad6de3ab0c2405458c690a
Claudio128 <cllaudio128@gmail.com>
2024-01-14 03:55:51 -0300

    Exercicio 02 - TS

---
+++ Exercicio2/index.js
@@ -0,0 +1,5 @@
+"use strict";
+const nmr = 35;
+const nmrtostring = nmr.toString();
+const frase = "Este é o número: " + nmrtostring;
+console.log(frase);
---
+++ Exercicio2/index.ts
@@ -0,0 +1,5 @@
+const nmr:number = 35
+const nmrtostring = nmr.toString()
+const frase:string = "Este é o número: " + nmrtostring
+
+console.log(frase)
