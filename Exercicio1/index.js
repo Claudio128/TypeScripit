c7ce664fac4507927dc9a9795cf75215edbe8da3
Claudio128 <cllaudio128@gmail.com>
2024-01-13 21:58:48 -0300

    Primeiro Exercício

---
+++ Exercicio1/index.js
@@ -0,0 +1,8 @@
+var a = 5;
+var b = 10;
+var nome = "Claudio";
+function adding(a, b) {
+    var soma = a + b;
+    console.log(soma);
+}
+adding(a, b);
---
+++ Exercicio1/index.ts
@@ -0,0 +1,10 @@
+const a = 5
+const b = 10
+const nome = "Claudio" 
+
+function adding(a:number, b:number) {
+    const soma = a + b 
+    console.log(soma)
+}
+
+adding(a, b)
