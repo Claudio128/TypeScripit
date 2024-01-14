0d803435eea1171f4c8c7169028c21168fe2018a
Claudio128 <cllaudio128@gmail.com>
2024-01-14 17:13:59 -0300

    Array em TS

---
+++ Aula06/index.js
@@ -0,0 +1,8 @@
+"use strict";
+let numbers = [1, 2, 3];
+numbers.push(6);
+console.log(numbers[2]);
+// numbers = "teste"
+const nomes = ["Claudio", "Mago"];
+// nomes.push(4)
+console.log(nomes);
---
+++ Aula06/index.ts
@@ -0,0 +1,13 @@
+let numbers: number[] = [1, 2, 3]
+
+numbers.push(6)
+
+console.log(numbers[2])
+
+// numbers = "teste"
+
+const nomes: string[] = ["Claudio", "Mago"]
+
+// nomes.push(4)
+
+console.log(nomes)
