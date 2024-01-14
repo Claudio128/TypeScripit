5abbe067c13fbf11677d797798037518c5ef04f6
Claudio128 <cllaudio128@gmail.com>
2024-01-13 23:46:28 -0300

    Fundamentos do TS - String

---
+++ Aula03/index.js
@@ -0,0 +1,7 @@
+var firstName = "Claudio";
+console.log(firstName.toUpperCase());
+var fullName;
+var lastName = "Andrade";
+fullName = firstName + " " + lastName;
+console.log(fullName);
+console.log(typeof fullName);
---
+++ Aula03/index.ts
@@ -0,0 +1,12 @@
+const firstName: string = "Claudio"
+
+console.log(firstName.toUpperCase())
+
+let fullName: string
+
+const lastName: string = "Andrade"
+
+fullName = firstName + " " + lastName
+
+console.log(fullName)
+console.log(typeof fullName)
