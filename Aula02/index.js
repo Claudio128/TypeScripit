184550d750c42ed9f2d0d46c6a7320ab0fab822a
Claudio128 <cllaudio128@gmail.com>
2024-01-13 21:59:43 -0300

    Fundamentos ao TS - Number

---
+++ Aula02/index.js
@@ -0,0 +1,7 @@
+var x = 10;
+console.log(x);
+x = 16;
+console.log(typeof x);
+var y = 15.584848;
+console.log(y);
+console.log(y.toPrecision(3));
---
+++ Aula02/index.ts
@@ -0,0 +1,12 @@
+let x: number = 10
+console.log(x)
+
+x = 16
+
+console.log(typeof x)
+
+const y:number = 15.584848
+
+console.log(y)
+
+console.log(y.toPrecision(3))
